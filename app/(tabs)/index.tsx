51e6c8cf71681b6199fd049f88d12ba7324fbd43
Shekhar-903 <shekharmaharana1@gmail.com>
2026-03-27 00:13:37 +0530

    Improved UI for index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
 import { useState } from 'react';
-import { View, Button } from 'react-native';
-
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import Login from '../(auth)/login';
 import Register from '../(auth)/register';
 
@@ -8,19 +9,60 @@ export default function Index() {
   const [isLogin, setIsLogin] = useState(true);
 
   return (
-    <View style={{ flex: 1 }}>
-      
-      {/* Toggle Buttons */}
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 50 }}>
-        <Button title="Login" onPress={() => setIsLogin(true)} />
-        <Button title="Register" onPress={() => setIsLogin(false)} />
-      </View>
+    <ThemedView style={styles.container}>
+      <ThemedView style={styles.toggleContainer}>
+        <TouchableOpacity 
+          style={[styles.toggleButton, isLogin && styles.activeTab]} 
+          onPress={() => setIsLogin(true)}
+        >
+          <ThemedText style={isLogin ? styles.activeText : styles.inactiveText}>Login</ThemedText>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          style={[styles.toggleButton, !isLogin && styles.activeTab]} 
+          onPress={() => setIsLogin(false)}
+        >
+          <ThemedText style={!isLogin ? styles.activeText : styles.inactiveText}>Register</ThemedText>
+        </TouchableOpacity>
+      </ThemedView>
 
-      {/* Show Screen */}
-      <View style={{ flex: 1 }}>
+      <ThemedView style={{ flex: 1 }}>
         {isLogin ? <Login /> : <Register />}
-      </View>
-
-    </View>
+      </ThemedView>
+    </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },
+  toggleContainer: {
+    flexDirection: 'row',
+    marginHorizontal: 20,
+    marginBottom: 20,
+    borderRadius: 12,
+    backgroundColor: '#f0f0f0', // You can use useThemeColor hook for this
+    padding: 4,
+  },
+  toggleButton: {
+    flex: 1,
+    paddingVertical: 10,
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  activeTab: {
+    backgroundColor: '#fff',
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  activeText: {
+    fontWeight: '600',
+  },
+  inactiveText: {
+    opacity: 0.6,
+  }
+});
