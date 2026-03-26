996a63ca3469f1346eedd618f6eddc298a004ff1
YashSharma789 <yks1382007@gmail.com>
2026-03-27 00:23:46 +0530

    fixed submodule issue

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+import { View, Button } from 'react-native';
+
 import Login from '../(auth)/login';
+import Register from '../(auth)/register';
 
 export default function Index() {
-  return <Login />;
+  const [isLogin, setIsLogin] = useState(true);
+
+  return (
+    <View style={{ flex: 1 }}>
+      
+      {/* Toggle Buttons */}
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 50 }}>
+        <Button title="Login" onPress={() => setIsLogin(true)} />
+        <Button title="Register" onPress={() => setIsLogin(false)} />
+      </View>
+
+      {/* Show Screen */}
+      <View style={{ flex: 1 }}>
+        {isLogin ? <Login /> : <Register />}
+      </View>
+
+    </View>
+  );
 }
